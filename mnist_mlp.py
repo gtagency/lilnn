7084be3205e84cd97d0eb182bf09ac09a67b104d
Diptodip Deb <dr.diptodip@gmail.com>
2017-09-13 16:55:12 -0400

    Consistent variable names

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -9,10 +9,10 @@ def label_to_bit_vector(labels, nbits):
 
     return bv
 
-def create_minibatches(data, labels, batch_size, create_bit_vector=False):
+def create_batches(data, labels, batch_size, create_bit_vector=False):
     N = data.shape[0]
     if N % batch_size != 0:
-        print( "Warning in create_minibatches(): Batch size {0} does not " \
+        print( "Warning in create_batches(): Batch size {0} does not " \
               "evenly divide the number of examples {1}.".format(batch_size,
                                                                  N))
     chunked_data = []
@@ -35,13 +35,13 @@ f = gzip.open('mnist.pkl.gz')
 train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 f.close()
 
-minibatch_size = 100
-train_data, train_labels = create_minibatches(train_set[0], train_set[1],
-                                              minibatch_size,
+batch_size = 100
+train_data, train_labels = create_batches(train_set[0], train_set[1],
+                                              batch_size,
                                               create_bit_vector=True)
-valid_data, valid_labels = create_minibatches(valid_set[0], valid_set[1],
-                                              minibatch_size,
+valid_data, valid_labels = create_batches(valid_set[0], valid_set[1],
+                                              batch_size,
                                               create_bit_vector=True)
 
-mlp = MLP(layer_dimensions=[784, 100, 100, 10], batch_size=minibatch_size)
+mlp = MLP(layer_dimensions=[784, 100, 100, 10], batch_size=batch_size)
 mlp.train(train_data, train_labels, valid_data, valid_labels)
